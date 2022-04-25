5d242ea2f65664b9e59f8348c2d301e3807185f7
Santhosh-user <85502994+Santhosh-user@users.noreply.github.com>
2022-04-25 18:00:56 +0530

    1

--- c3/c3-1/src/App.jsx
+++ c3/c3-1/src/App.jsx
@@ -7,7 +7,6 @@ import { Admin } from "./components/Admin";
 import { Navbar } from "./components/Navbar";
 import { Logout } from "./components/Logout";
 import {Route, Routes} from "react-router-dom"
-import { ProtectedRoute } from "./components/ProtectedRoute";
 
 function App() {
   return (
@@ -16,8 +15,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Home></Home>} />
         <Route path="/employees" element={<EmployeeList></EmployeeList>}/>
-        <Route path="/employees/:id" element={<ProtectedRoute><EmployeeDetails></EmployeeDetails></ProtectedRoute>}/>
-        <Route path="/admin" element={<Admin></Admin>}/>
         <Route path="/login" element={<Login></Login>}/>
         <Route path="/logout" element={<Logout></Logout>}/>
       </Routes>
