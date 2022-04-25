fec2df07981a00857360c3d1fb2e8c3bd485917b
Santhosh-user <85502994+Santhosh-user@users.noreply.github.com>
2022-04-25 17:30:32 +0530

    1

--- c3/c3-1/src/components/EmployeeList.jsx
+++ c3/c3-1/src/components/EmployeeList.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios"
 import { useNavigate } from "react-router-dom";
-import { useContext } from "react/cjs/react.production.min";
+import { useContext } from "react";
 import { AuthAcc } from "./AuthAcc";
 
 
